8c8f51227a357f908e09b4a8fc66af93d68360ad
Bideo Wego <bideowego@gmail.com>
2017-10-04 01:01:35 -0400

    Add events and dom example

---
+++ 4_events_and_dom/script.js
@@ -0,0 +1,40 @@
+// Objects
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.greet = function() {
+    return `Hello world! My name is ${ this.name } and I'm ${ this.age } years old.`;
+  };
+}
+
+
+var people = [
+  new Person('George', 20),
+  new Person('Georgia', 21)
+];
+
+
+// Behavior
+
+$(function() {
+  var $people = $('#people');
+  people.forEach(function(person) {
+    var $link = $('<a href="#">').text(person.name);
+    $link.click(function(e) {
+      e.preventDefault();
+      var greeting = person.greet();
+      alert(greeting);
+      return false;
+    });
+    var $person = $('<li>').addClass('list-item');
+    $person.append($link);
+    $people.append($person);
+  });
+});
+
+
+
+
+
+
